3795789d61bf23262d56f343ffb315acba9ce2a1
ayrtondenner <ayrtondenner_2013@hotmail.com>
2017-07-13 02:43:51 -0300

    Correção para o Heroku.

--- config/initializers/database_initializer.rb
+++
@@ -1,6 +0,0 @@
-#busca = Users.find_by(:username: "admin")
-conta_admin = Account.find_by_username("admin")
-
-if (conta_admin == nil)
-  Account.new(:username => "admin", :password => "admin", :name => "admin").save()
-end
